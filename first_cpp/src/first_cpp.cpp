bf92e98fbe8af019bf37a3bf2e540279b760fe8b
root <root@9624919235a6>
2020-09-18 14:46:36 +0700

    helloefv

---
+++ first_cpp/src/first_cpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : first_cpp.cpp
+// Author      : karan bhatia
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "!HlloWnd" << endl;  // prints !!!Hello World!!!
+  return 0;
+}
